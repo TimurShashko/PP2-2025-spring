5ff184798e186222e31cab79f5aef4b16fafd34b
TimurShashko <cahbcotkyotday@gmail.com>
2025-03-16 09:49:37 +0500

    dodelal

--- lab7/sounds/2.py
+++ lab7/sounds/2.py
@@ -9,15 +9,30 @@ running = True
 
 clock = pygame.time.Clock()
 
-_sound_library = dict()
-def play_sound(path):
-    global _sound_library
-    sound = _sound_library.get(path)
-    if sound is None:
-        canonicalized_path = path.replace('/', os.sep).replace('\\', os.sep)
-        sound = pygame.mixer.Sound(canonicalized_path)
-        _sound_library[path] = sound
-    sound.play()
+songs = [
+    "Miyagi_JEndshpil_Mav-d_-_Aphrodisiac_78725692.mp3",
+    "MiyaGi_Andy_Panda_-_Tantra_70276518.mp3",
+    "Twenty_One_Pilots_-_Stressed_Out_47828699.mp3"
+]
+
+current_song_index = 0
+
+def play_song(index):
+    pygame.mixer.music.load(songs[index])
+    pygame.mixer.music.play()
+
+def stop_music():
+    pygame.mixer.music.stop()
+
+def next_song():
+    global current_song_index
+    current_song_index = (current_song_index + 1) % len(songs)
+    play_song(current_song_index)
+
+def previous_song():
+    global current_song_index
+    current_song_index = (current_song_index - 1) % len(songs)
+    play_song(current_song_index)
 
 while running:
     for event in pygame.event.get():
@@ -25,11 +40,17 @@ while running:
             running = False
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_p:
-                play_sound("Miyagi_JEndshpil_Mav-d_-_Aphrodisiac_78725692.mp3")
+                play_song(current_song_index)
             if event.key == pygame.K_s:
-                pygame.mixer.music.stop()
+                stop_music()
+            if event.key == pygame.K_n:
+                next_song()
+            if event.key == pygame.K_b:
+                previous_song()
         
     screen.fill("white")
 
     pygame.display.flip()
     clock.tick(60)
+
+pygame.quit()
